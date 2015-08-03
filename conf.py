260c0e6dc98bae502467051e6caea907f538fcb6
Andreas Maier <maiera@de.ibm.com>
2015-08-03 18:24:35 +0200

    Updated list of exclude directories for Breathe.

--- conf.py
+++ conf.py
@@ -83,7 +83,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['sphinx/build', 'sphinx-ext']
+exclude_patterns = ['doxygen', 'sphinx', 'sphinx-ext']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
